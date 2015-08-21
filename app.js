6f55df2a23b5641ff2c6a5f3af8743fa66bb655c
Travis Noll <tjn@oit-tjn01.oit.umn.edu>
2015-08-21 10:41:50 -0500

    bot controller

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 
+var bot = require ('./bot_ctl');
+
+console.log(bot);
+
 app.set('view engine', 'jade');
 app.use('/assets', express.static('assets'));
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Particle Remote', message: 'Hello there!'});
+});
+
+app.get('/controls/:control', function (req, res) {
+	console.log(req.params.control);
+	bot.processDirection(req.params.control);
 });
 
 var server = app.listen(3000, function () {
