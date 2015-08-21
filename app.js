515b59a8ee4514aaacc98724c91c1bafbec4077f
blackjk3 <blackjk@gmail.com>
2015-08-21 10:05:53 -0500

    Adding styles

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'jade');
+app.use('/assets', express.static('assets'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', { title: 'Particle Remote', message: 'Hello there!'});
 });
 
 var server = app.listen(3000, function () {
